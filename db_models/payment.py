ba101991a4fbe600bffc35b142d8c0f9d42ca83f
guimileib <guilhermemileib@gmail.com>
2024-12-03 17:33:45 -0300

    Implementando rota de criação de pafamentos

--- db_models/payment.py
+++ db_models/payment.py
@@ -10,3 +10,13 @@ class Payment(db.Model):
     bank_payment_id = db.Column(db.Integer, nullable=True) # Não obrigatório
     qr_code = db.Column(db.String(100), nullable=True)
     expiration_date = db.Column(db.DateTime) # 2024-01-01 -> Armazena a Hora
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "value": self.value,
+            "paid": self.paid,
+            "bank_payment_id": self.bank_payment_id,
+            "qr_code": self.qr_code,
+            "expiration_date": self.expiration_date
+        }
